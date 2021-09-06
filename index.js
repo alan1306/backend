791ce355f50ba14875d8e33347758241d56b278a
Alan joseph saji <alan130625@gmail.com>
2021-09-06 22:38:56 +0530

    changed

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if(!config.get('jwtPrivateKey')){
     console.error("FATAL ERROR jwt token not defined");
     process.exit(1);
 }
-mongoose.connect('mongodb://localhost/EduLearn')
+mongoose.connect('mongodb+srv://alan_admin_user_13:Alan130625@cluster0.j7bwp.mongodb.net/EduLearn?retryWrites=true&w=majority')
     .then(()=>console.log('connectes to database'))
     .catch(err =>console.log('error connecting',err))
 app.use(express.static('public'));
